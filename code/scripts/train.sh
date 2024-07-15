a1ac57d6c01535d4e9841e2e897c0501cced1f00
= <haitham.alhaji@duhokcihan.edu.krd>
2024-07-15 13:02:16 +0300

    u

--- code/scripts/train.sh
+++ code/scripts/train.sh
@@ -5,8 +5,8 @@ state_epoch=1
 pretrained_model_path='./saved_models/data/model_save_file'
 log_dir='new'
 
-multi_gpus=False
-mixed_precision=False
+multi_gpus=True
+mixed_precision=True
 
 nodes=8
 num_workers=8
