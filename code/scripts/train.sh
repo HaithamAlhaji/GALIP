d92b64416cf147849d37652efb21a0662291a8f1
= <haitham.alhaji@duhokcihan.edu.krd>
2024-07-15 13:14:08 +0300

    a

--- code/scripts/train.sh
+++ code/scripts/train.sh
@@ -14,12 +14,9 @@ master_port=11266
 stamp=gpu${nodes}MP_${mixed_precision}
 
 python /content/GALIP/code/src/train.py \
-    --stamp $stamp \
     --cfg $cfg \
-    --mixed_precision $mixed_precision \
     --log_dir $log_dir \
     --batch_size $batch_size \
     --state_epoch $state_epoch \
     --num_workers $num_workers \
-    --multi_gpus $multi_gpus \
     --pretrained_model_path $pretrained_model_path \
